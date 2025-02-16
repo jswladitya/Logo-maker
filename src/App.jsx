718b51d537d9da068946767a7c259f6ca16173e1
jswladitya <jswladitya0403@gmail.com>
2025-02-16 10:13:30 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Button } from "./components/ui/button"
+function App() {
+  
+
+  return (
+  <>
+    <h1>Hello world</h1>
+    <Button> Hello </Button>
+  </>
+  )
+}
+
+export default App
